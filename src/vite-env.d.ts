f718c26a71167de88a513a853e427e519e0832cd
David Almeida <contato.davidalmeida@outlook.com>
2024-05-09 11:08:32 -0300

    🏷️ Add or update types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface Project {
+  name: string;
+  description: string;
+  tags: string[];
+  madeIn: string;
+  links: Link[];
+}
+
+interface Link {
+  name: string;
+  url: string;
+  icon: React.ReactNode;
+}
