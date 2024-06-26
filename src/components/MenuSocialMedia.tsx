df2a0bd03935601d92a22c1dcacd2e8ef4eab952
David Almeida <contato.davidalmeida@outlook.com>
2024-06-26 15:23:00 -0300

    ✅ Update: MenuSocialMedia component

--- src/components/MenuSocialMedia.tsx
+++ src/components/MenuSocialMedia.tsx
@@ -1,12 +1,10 @@
+import * as Fa from "react-icons/fa6";
+import * as Io from "react-icons/io";
 import clsx from "clsx";
-import * as Fa from "react-icons/fa";
-
-interface MenuSocialMediaProps {
-  flexible: boolean;
-}
+import { MenuSocialMediaProps } from "../vite-env";
 
 export function MenuSocialMedia({ flexible }: MenuSocialMediaProps) {
-  const socialMedias: Link[] = [
+  const socialMedias = [
     {
       name: "TikTok",
       url: "https://www.tiktok.com/@davidalmeidadev",
@@ -18,9 +16,9 @@ export function MenuSocialMedia({ flexible }: MenuSocialMediaProps) {
       icon: <Fa.FaInstagram size={20} />,
     },
     {
-      name: "WhatsApp",
-      url: "https://wa.me/5586988641961",
-      icon: <Fa.FaWhatsapp size={20} />,
+      name: "Signal",
+      url: "https://signal.me/#eu/OCa5yL5EtHtyByR9zgHaENZ20v4PRKAJIjfpbvqm1qTYZUXVHLpHvJX-ag5sz2OJ",
+      icon: <Fa.FaSignalMessenger size={20} />,
     },
     {
       name: "LinkedIn",
@@ -33,19 +31,37 @@ export function MenuSocialMedia({ flexible }: MenuSocialMediaProps) {
     <ul
       className={clsx(
         "flex flex-col space-y-2",
-        flexible && "md:flex-row md:items-center md:space-x-4 md:space-y-0",
+        flexible && "md:flex-row md:items-center md:space-y-0",
       )}
     >
       {socialMedias.map((media, index) => (
-        <li key={index}>
+        <li
+          key={index}
+          className={clsx(
+            "pt-1 pr-11 bg-gray-900 text-gray-500",
+            !flexible && "bg-transparent text-gray-900",
+            index === 0 && "with-before",
+          )}
+        >
           <a
             href={media.url}
-            className="flex justify-start items-center gap-1 outline-none"
+            className="flex justify-start items-center gap-1 font-mono group"
             target="_blank"
             rel="noopener noreferrer"
             referrerPolicy="no-referrer"
           >
-            {media.icon} {media.name}
+            <span
+              className={
+                "flex justify-start items-center gap-1 transition-all ease-[cubic-bezier(.5,.12,.46,.88)] duration-300 transform -translate-x-full opacity-0 group-hover:opacity-100 group-hover:translate-x-0 relative -z-40 group-hover:z-10 group-hover:text-white"
+              }
+            >
+              <Io.IoIosArrowRoundForward size={20} /> {media.icon} {media.name}
+            </span>
+            <span
+              className={`flex justify-start items-center gap-1 transition-all ease-[cubic-bezier(.5,.12,.46,.88)] duration-500 transform group-hover:translate-y-4 group-hover:opacity-0 absolute z-10`}
+            >
+              {media.icon} {media.name}
+            </span>
           </a>
         </li>
       ))}
