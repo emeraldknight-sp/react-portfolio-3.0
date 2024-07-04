49a6175306b4079066d214b1f4ca38fbcc61d922
David Almeida <contato.davidalmeida@outlook.com>
2024-07-04 08:33:54 -0300

    🤡 Mock things

--- src/mock/menu.ts
+++ src/mock/menu.ts
@@ -1,5 +1,7 @@
-export const menu = [
-  { text: "Início", url: "start", acesskey: "s" },
+import { MenuOption } from "../vite-env";
+
+export const menu: MenuOption[] = [
+  { text: "Início", url: "home", acesskey: "h" },
   { text: "Sobre", url: "about", acesskey: "a" },
   { text: "Projetos", url: "projects", acesskey: "p" },
   { text: "Contato", url: "contact", acesskey: "c" },
---
+++ src/mock/services.ts
@@ -0,0 +1,7 @@
+export const services: String[] = [
+  "Criação de websites que oferecem uma experiência consistente e intuitiva em todos os dispositivos.",
+  "Orientação especializada para melhorar a experiência do usuário e a interface do usuário de seus projetos.",
+  "Serviços de suporte para garantir que seu site permaneça atualizado e funcione perfeitamente ao longo do tempo.",
+  "Garantia de que cada site desenvolvido não só seja bonito, mas também rápido, acessível e fácil de navegar.",
+  "Implemento a conexão entre sistemas através de integrações de APIs eficientes, melhorando a funcionalidade e a automação de suas aplicações.",
+];
